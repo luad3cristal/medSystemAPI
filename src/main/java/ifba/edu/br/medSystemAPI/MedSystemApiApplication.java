2975fd593eb6e0ce1444e6b8d8e5a6c1055f1e4d
Luana Lima <luanalima.2006@hotmail.com>
2025-10-25 23:09:39 -0300

    initial commit

---
+++ src/main/java/ifba/edu/br/medSystemAPI/MedSystemApiApplication.java
@@ -0,0 +1,13 @@
+package ifba.edu.br.medSystemAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedSystemApiApplication.class, args);
+	}
+
+}
