ddb2a4ed3f9744176a114a6529743513eec53f4f
Luana Lima <luanalima.2006@hotmail.com>
2025-11-01 20:54:24 -0300

    feat: create doctor entity

---
+++ src/main/java/ifba/edu/br/medSystemAPI/models/entities/Doctor.java
@@ -0,0 +1,65 @@
+package ifba.edu.br.medSystemAPI.models.entities;
+
+import ifba.edu.br.medSystemAPI.models.enums.Specialty;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "doctors")
+public class Doctor {
+  @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+  private String email;
+  private String phone;
+  
+  @Column(unique = true)
+  private String crm;
+
+  @Enumerated(EnumType.STRING)
+  private Specialty specialty;
+
+  @ManyToOne
+  @JoinColumn(name = "address_id")
+  private Address address;
+
+  public Doctor () {}
+
+  public Doctor (String name, String email, String phone, String crm, Specialty specialty, Address address) {
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+    this.crm = crm;
+    this.specialty = specialty;
+    this.address = address;
+  }
+
+  public String getName() { return name; }
+  public void setName(String name) { this.name = name; }
+
+  public String getEmail() { return email; }
+  public void setEmail(String email) { this.email = email; }
+
+  public String getPhone() { return phone; }
+  public void setPhone(String phone) { this.phone = phone; }
+
+  public String getCRM() { return crm; }
+  public void setCRM(String crm) { this.crm = crm; }
+
+  public Specialty getCity() { return specialty; }
+  public void setCity(Specialty specialty) { this.specialty = specialty; }
+
+  public Address getAddress() { return address; }
+  public void setAddress(Address address) { this.address = address; }
+}
