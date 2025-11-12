559f5b9a83faba7ae5b9ab889a62e50a0736509b
Luana Lima <luanalima.2006@hotmail.com>
2025-11-11 22:49:07 -0300

    fix: add complementary getter and setter

--- src/main/java/ifba/edu/br/medSystemAPI/models/entities/Address.java
+++ src/main/java/ifba/edu/br/medSystemAPI/models/entities/Address.java
@@ -34,6 +34,9 @@ public class Address {
     this.zipCode = zipCode;
   }
 
+  public Long getId() { return id; }
+  public void setId(Long id) { this.id = id; }
+
   public String getStreet() { return street; }
   public void setStreet(String street) { this.street = street; }
 
