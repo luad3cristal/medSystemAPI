1b5c91f60d1db181ecdf93ec62e27ce7de850b2d
Luana Lima <luanalima.2006@hotmail.com>
2025-11-22 20:14:13 -0300

    feat: implement query

--- src/main/java/ifba/edu/br/medSystemAPI/repositories/DoctorRepository.java
+++ src/main/java/ifba/edu/br/medSystemAPI/repositories/DoctorRepository.java
@@ -3,11 +3,12 @@ package ifba.edu.br.medSystemAPI.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import ifba.edu.br.medSystemAPI.models.entities.Doctor;
 
 public interface DoctorRepository extends JpaRepository <Doctor, Long> {
-    // TODO: Adicionar @Query para buscar por status corretamente
-    // TODO: Tratar casos onde não há médicos ativos (Page vazia)
-    Page<Doctor> findAllActive(Pageable pageable, Boolean status);
+    @Query("SELECT d from Doctor d WHERE d.status = :status")
+    Page<Doctor> findByStatus(Pageable pageable, @Param("status") Boolean status);
 }
