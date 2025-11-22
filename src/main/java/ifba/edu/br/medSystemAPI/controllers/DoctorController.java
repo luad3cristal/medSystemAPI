65f0331a6cebb13b162403665440b712e38162b8
Luana Lima <luanalima.2006@hotmail.com>
2025-11-22 17:03:09 -0300

    feat: create controllers files

---
+++ src/main/java/ifba/edu/br/medSystemAPI/controllers/DoctorController.java
@@ -0,0 +1,10 @@
+package ifba.edu.br.medSystemAPI.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/doctors")
+public class DoctorController {
+  
+}
---
+++ src/main/java/ifba/edu/br/medSystemAPI/controllers/PatientController.java
@@ -0,0 +1,10 @@
+package ifba.edu.br.medSystemAPI.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+  
+}
