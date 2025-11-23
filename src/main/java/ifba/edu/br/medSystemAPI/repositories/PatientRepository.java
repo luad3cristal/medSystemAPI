60469e3cf4384fa6574f08be495ad30d47480e9f
Luana Lima <luanalima.2006@hotmail.com>
2025-11-23 01:35:07 -0300

    feat: add custom query method to PatientRepository for fetching patients by status with pagination

--- src/main/java/ifba/edu/br/medSystemAPI/repositories/PatientRepository.java
+++ src/main/java/ifba/edu/br/medSystemAPI/repositories/PatientRepository.java
@@ -1,9 +1,15 @@
 package ifba.edu.br.medSystemAPI.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import ifba.edu.br.medSystemAPI.models.entities.Patient;
 
 public interface PatientRepository extends JpaRepository <Patient, Long> {
-    
+    // TODO: Considerar usar método de convenção: findByStatus(Boolean status, Pageable pageable)
+    @Query("SELECT p from Patient p WHERE p.status = :status")
+    Page<Patient> findByStatus(Pageable pageable, @Param("status") Boolean status);
 }
