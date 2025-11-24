eb470fbbcbcccaec1ab368e598acfcba1cec7f51
Luana Lima <luanalima.2006@hotmail.com>
2025-11-24 01:49:17 -0300

    feat: add method to find available doctors at a specific appointment time

--- src/main/java/ifba/edu/br/medSystemAPI/repositories/DoctorRepository.java
+++ src/main/java/ifba/edu/br/medSystemAPI/repositories/DoctorRepository.java
@@ -1,5 +1,8 @@
 package ifba.edu.br.medSystemAPI.repositories;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +14,11 @@ import ifba.edu.br.medSystemAPI.models.entities.Doctor;
 public interface DoctorRepository extends JpaRepository <Doctor, Long> {
     @Query("SELECT d from Doctor d WHERE d.status = :status")
     Page<Doctor> findByStatus(Pageable pageable, @Param("status") Boolean status);
+
+    @Query(
+        "SELECT d from Doctor d WHERE d.id NOT IN" +
+        "(SELECT a.doctor.id FROM Appointment a)" +
+        "WHERE a.appointmentTime = :appointmentTime AND a.status = 'SCHEDULED')"
+    )
+    List<Doctor> findAvailableDoctorsAtAppointmentTime(@Param("appointmentTime") LocalDateTime appointmentTime);
 }
