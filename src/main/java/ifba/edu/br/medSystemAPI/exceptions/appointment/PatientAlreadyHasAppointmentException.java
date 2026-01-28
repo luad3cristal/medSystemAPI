a6c539f0b1bb771269fccb0584e46323c4bc13b8
Luana Lima <luanalima.2006@hotmail.com>
2026-01-27 22:59:05 -0300

    refactor: remove TODO comment

--- src/main/java/ifba/edu/br/medSystemAPI/exceptions/appointment/PatientAlreadyHasAppointmentException.java
+++ src/main/java/ifba/edu/br/medSystemAPI/exceptions/appointment/PatientAlreadyHasAppointmentException.java
@@ -6,7 +6,6 @@ import ifba.edu.br.medSystemAPI.models.entities.Patient;
 
 public class PatientAlreadyHasAppointmentException extends RuntimeException {
   public PatientAlreadyHasAppointmentException(Patient patient, LocalDate date) {
-    //TODO: passar o nome ao invés do id
     super("Patient " + patient.getName() + "(CPF: " + patient.getCPF() + ") already has an appointment on " + date);
   }
   
